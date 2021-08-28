104f1c955833d67a8f73c0cca497c39b360014a5
Emmanuel Murairi <emurairi18@alastudents.org>
2021-08-28 10:56:52 +0400

    submit

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -4,18 +4,19 @@ const request = require('request');
 const endpoint = 'https://swapi-api.hbtn.io/api';
 const filmId = process.argv[2];
 
-request(`${endpoint}/films/${filmId}/`, (error, response, body) => {
-  if (error) {
-    console.log(error);
-  } else {
-    let characters = JSON.parse(body).characters;
+request(`${endpoint}/films/${filmId}/`, async function (error, response, body) {
+  if (error) return console.log(error);
 
-    characters.forEach((character) => {
+  let characters = JSON.parse(body).characters;
+
+  for (const character of characters) {
+    await new Promise((resolve, reject) => {
       request(character, (error, response, body) => {
         if (error) {
-          console.log(error);
+          reject(error);
         } else {
           console.log(JSON.parse(body).name);
+          resolve(body);
         }
       });
     });
